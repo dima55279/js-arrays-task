1f014dde8869c77695a3c7b3d26ef8d675c0ab81
dima55279 <dima55279@yandex.ru>
2025-03-11 15:52:52 +0300

    Выполнено задание 3.

--- solutions/3-isset.js
+++ solutions/3-isset.js
@@ -1,3 +1,8 @@
 // BEGIN
-
+export const get = (array, index, defaultValue = null) => {
+    if (index < 0 || index > array.length - 1) {
+        return defaultValue;
+    }
+    return array[index];
+}
 // END
