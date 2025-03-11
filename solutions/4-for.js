10a4925802db4997704c6c7eefa7a5096c598ed0
dima55279 <dima55279@yandex.ru>
2025-03-11 15:53:10 +0300

    Выполнено задание 4.

--- solutions/4-for.js
+++ solutions/4-for.js
@@ -1,3 +1,10 @@
 // BEGIN
-
+const addPrefix = (array, prefix) => {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(prefix + " " + array[i]);
+    }
+    return newArray;
+}
+export default addPrefix;
 // END
