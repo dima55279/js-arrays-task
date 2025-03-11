5f115c24cc23203b4c6b7b8bad075c7b82f51d29
dima55279 <dima55279@yandex.ru>
2025-03-11 15:57:03 +0300

    Выполнено задание 18. Закончена ЛР 2.

--- solutions/18-spread-operator.js
+++ solutions/18-spread-operator.js
@@ -1,3 +1,14 @@
 // BEGIN
-
+export const flatten = (array) => {
+    const newArray = []
+    if (array.length > 0) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i].length >= 0) {
+                newArray.push(...array[i]);
+            }
+            else newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
 // END
