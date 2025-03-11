fb3cfb2457d2130f308a5baf4c6db1e148d0f819
dima55279 <dima55279@yandex.ru>
2025-03-11 15:56:03 +0300

    Выполнено задание 15.

--- solutions/15-sorting.js
+++ solutions/15-sorting.js
@@ -1,3 +1,18 @@
 // BEGIN
-
+const bubbleSort = (array) => {
+    let flag = true;
+    let nonRearrangedElements = array.length;
+    while (flag && nonRearrangedElements > 0) {
+        for (let i = 0; i < nonRearrangedElements - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                let temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+        nonRearrangedElements -= 1;
+    }
+    return array;
+}
+export default bubbleSort;
 // END
