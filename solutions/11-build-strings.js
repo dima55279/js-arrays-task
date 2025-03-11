0f08bdea68d3cb6c5e46a9800b5698d3d64f78e6
dima55279 <dima55279@yandex.ru>
2025-03-11 15:54:55 +0300

    Выполнено задание 11.

--- solutions/11-build-strings.js
+++ solutions/11-build-strings.js
@@ -1,3 +1,14 @@
 // BEGIN
-
+const buildDefinitionList = (array) => {
+    let string = "";
+    if (array.length > 0) {
+        for (let i = 0; i < array.length; i++) {
+            string += "<dt>" + array[i][0] + "</dt>" + "<dd>" + array[i][1] + "</dd>";
+        }
+    }
+    else return string;
+    string = "<dl>" + string + "</dl>";
+    return string;
+}
+export default buildDefinitionList;
 // END
