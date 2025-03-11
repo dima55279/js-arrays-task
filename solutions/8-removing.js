a5f2be2d5d4c203f63f217419dcf8dbd36dcefdc
dima55279 <dima55279@yandex.ru>
2025-03-11 15:54:11 +0300

    Выполнено задание 8.

--- solutions/8-removing.js
+++ solutions/8-removing.js
@@ -1,3 +1,12 @@
 // BEGIN
-
+const getSameParity = (array) => {
+    let parityOfFirstElement = Math.abs(array[0]) % 2;
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (Math.abs(array[i]) % 2 === parityOfFirstElement)
+            newArray.push(array[i]);
+    }
+    return newArray;
+}
+export default getSameParity;
 // END
