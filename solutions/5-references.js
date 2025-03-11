e94a3f5a33c812983cb5d69d26668343f066b949
dima55279 <dima55279@yandex.ru>
2025-03-11 15:53:25 +0300

    Выполнено задание 5.

--- solutions/5-references.js
+++ solutions/5-references.js
@@ -1,3 +1,12 @@
 // BEGIN
-
+export const reverse = (array) => {
+    let newArray = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        newArray.push(array[i]);
+    }
+    for (let i = 0; i < array.length; i++) {
+        array[i] = newArray[i];
+    }
+    return array;
+}
 // END
