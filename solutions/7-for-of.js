b7e938943a0bc91a62b7f773119b934dd8cdcb0e
dima55279 <dima55279@yandex.ru>
2025-03-11 15:53:55 +0300

    Выполнено задание 7.

--- solutions/7-for-of.js
+++ solutions/7-for-of.js
@@ -1,3 +1,14 @@
 // BEGIN
-
+const calculateAverage = (array) => {
+    let sum = 0;
+    if (array.length > 0) {
+        for (const temperature of array) {
+            sum += temperature;
+        }
+        sum = sum / array.length;
+        return sum;
+    }
+    else return null;
+}
+export default calculateAverage;
 // END
