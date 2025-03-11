330e82e184d5cad875d6ffd645c92327b8380dd8
dima55279 <dima55279@yandex.ru>
2025-03-11 15:54:27 +0300

    Выполнено задание 9.

--- solutions/9-control-statements.js
+++ solutions/9-control-statements.js
@@ -1,3 +1,12 @@
 // BEGIN
-
+const getTotalAmount = (array, currency) => {
+    let balance = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].slice(0, 3) === currency) {
+            balance += Number(array[i].slice(4));
+        }
+    }
+    return balance;
+}
+export default getTotalAmount;
 // END
