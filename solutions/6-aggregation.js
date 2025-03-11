3cf0bfcd597db99bce56127dc7af2f14f4b8bfd5
dima55279 <dima55279@yandex.ru>
2025-03-11 15:53:41 +0300

    Выполнено задание 6.

--- solutions/6-aggregation.js
+++ solutions/6-aggregation.js
@@ -1,3 +1,10 @@
 // BEGIN
-
+export const calculateSum = (array) => {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 3 === 0)
+            sum += array[i];
+    }
+    return sum;
+}
 // END
