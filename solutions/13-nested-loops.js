14c160a15abf65bcdf8f5f867e8ac6d66b8b0574
dima55279 <dima55279@yandex.ru>
2025-03-11 15:55:33 +0300

    Выполнено задание 13.

--- solutions/13-nested-loops.js
+++ solutions/13-nested-loops.js
@@ -1,5 +1,18 @@
 const _ = require("lodash"); 
 
 // BEGIN
-
+const getSameCount = (firstArray, secondArray) => {
+    let uniqueFirstArray = _.uniq(firstArray);
+    let uniqueSecondArray = _.uniq(secondArray);
+    let counter = 0;
+    for (let i = 0; i < uniqueFirstArray.length; i++) {
+        for (let j = 0; j < uniqueSecondArray.length; j++) {
+            if (uniqueFirstArray[i] === uniqueSecondArray[j]) {
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
+export default getSameCount;
 // END
