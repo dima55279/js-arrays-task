2caebdbb642559fbded73b4cf58dd7ec97ac384c
dima55279 <dima55279@yandex.ru>
2025-03-11 15:56:24 +0300

    Выполнено задание 16.

--- solutions/16-destructuring.js
+++ solutions/16-destructuring.js
@@ -6,5 +6,21 @@ const getDistance = ([x1, y1], [x2, y2]) => {
   };
 
 // BEGIN
-
+export const getTheNearestLocation = (locations, coordinates) => {
+    if (locations.length !== 0) {
+        let [place, [x, y]] = locations[0];
+        let nearestDistance = getDistance([x, y], coordinates);
+        for (let i = 1; i < locations.length; i++) {
+            let [tempPlace, [tempX, tempY]] = locations[i];
+            if (nearestDistance > getDistance([tempX, tempY], coordinates)) {
+                place = tempPlace;
+                x = tempX;
+                y = tempY;
+                nearestDistance = getDistance([x, y], coordinates);
+            }
+        }
+        return [place, [x, y]];
+    }
+    else return null;
+}
 // END
