f115337348b915c5ae5628e04f1ccec156a02c82
dima55279 <dima55279@yandex.ru>
2025-03-11 15:52:22 +0300

    Выполнено задание 2.

--- solutions/2-modification.js
+++ solutions/2-modification.js
@@ -1,3 +1,10 @@
 // BEGIN
-
+export const swap = (array) => {
+    let firstElement = array[0];
+    if (array.length > 1) {
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = firstElement;
+    }
+    return array;
+}
 // END
