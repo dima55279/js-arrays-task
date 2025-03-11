f0bfa2cf41f6619966a4d36e563d1607d65c280f
dima55279 <dima55279@yandex.ru>
2025-03-11 15:51:11 +0300

    Выполнено задание 1.

--- solutions/1-syntax.js
+++ solutions/1-syntax.js
@@ -1,3 +1,10 @@
 // BEGIN (write your solution here)
-
+export const getWeekends = (param = "long") => {
+    if (param === "long") {
+        return ["saturday", "sunday"]
+    }
+    if (param === "short") {
+        return ["sat", "sun"]
+    }
+}
 // END
