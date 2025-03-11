e72f1709037feb5a327380901ca45af516c69ade
dima55279 <dima55279@yandex.ru>
2025-03-11 15:56:36 +0300

    Выполнено задание 17.

--- solutions/17-rest-operator.js
+++ solutions/17-rest-operator.js
@@ -1,3 +1,15 @@
 // BEGIN
-
+export const getMax = (array) => {
+    if (array.length > 0) {
+        const [first, ... rest] = array;
+        let max = first;
+        for (let i = 0; i < rest.length; i++) {
+            if (max < rest[i]) {
+                max = rest[i];
+            }
+        }
+        return max;
+    }
+    else return null;
+}
 // END
