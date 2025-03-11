f9d2ad8cf9991e1dcfb32c9d45c6633d1fd14126
dima55279 <dima55279@yandex.ru>
2025-03-11 15:54:41 +0300

    Выполнено задание 10.

--- solutions/10-nested-arrays.js
+++ solutions/10-nested-arrays.js
@@ -1,3 +1,20 @@
 // BEGIN
-
+const getSuperSeriesWinner = (array) => {
+    let canada = 0;
+    let ussr = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i][0] > array[i][1]) {
+            canada++;
+        }
+        else if (array[i][0] < array[i][1]) {
+            ussr++;
+        }
+    }
+    if (canada > ussr)
+        return "canada";
+    else if (canada < ussr)
+        return "ussr";
+    else return null;
+}
+export default getSuperSeriesWinner;
 // END
