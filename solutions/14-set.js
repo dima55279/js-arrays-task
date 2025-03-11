be67581bbec30cdf56126de482f183f51aec1b0b
dima55279 <dima55279@yandex.ru>
2025-03-11 15:55:51 +0300

    Выполнено задание 14.

--- solutions/14-set.js
+++ solutions/14-set.js
@@ -1,3 +1,12 @@
 // BEGIN
-
+const countUniqChars = (string) => {
+    let arrayOfUniqueChars = [];
+    for (let i = 0; i < string.length; i++) {
+        if (arrayOfUniqueChars.includes(string[i]) !== true) {
+            arrayOfUniqueChars.push(string[i]);
+        }
+    }
+    return arrayOfUniqueChars.length;
+}
+export default countUniqChars;
 // END
