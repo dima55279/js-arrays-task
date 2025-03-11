fcb74c05ee31a36027b2e55381e9a0a03927f8d7
dima55279 <dima55279@yandex.ru>
2025-03-11 15:55:17 +0300

    Выполнено задание 12.

--- solutions/12-strings.js
+++ solutions/12-strings.js
@@ -1,3 +1,15 @@
 // BEGIN
-
+const makeCensored = (sentence, banwords) => {
+    let censorship = "$#%!"
+    let sentenceAsArray = sentence.split(' ');
+    for (let i = 0; i < banwords.length; i++) {
+        for (let j = 0; j < sentenceAsArray.length; j++) {
+            if (sentenceAsArray[j] === banwords[i]) {
+                sentenceAsArray[j] = censorship;
+            }
+        }
+    }
+    return sentenceAsArray.join(' ');
+}
+export default makeCensored;
 // END
